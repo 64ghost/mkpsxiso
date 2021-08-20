e7a1465a95c81f8eb5efbcc711d47ac586b7128b
64ghost <77530734+64ghost@users.noreply.github.com>
2021-08-20 17:29:48 +0400

    Add files via upload

---
+++ src/global.h
@@ -0,0 +1,15 @@
+#ifndef _GLOBAL_H
+#define _GLOBAL_H
+
+#include <sys/time.h>
+
+namespace global {
+
+	extern time_t	BuildTime;
+	extern int		QuietMode;
+	extern int		noXA;
+	extern int		NoLimit;
+	extern int		trackNum;
+};
+
+#endif // _GLOBAL_H
